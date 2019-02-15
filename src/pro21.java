ad28e361b20e3d88e61ced967d2296fc9c66ac80
JDawnF <335825732@qq.com>
2019-02-15 20:45:29 +0800

    合并两个有序链表

---
+++ src/pro21.java
@@ -0,0 +1,49 @@
+/**
+ * @program: leetcode
+ * @author: baichen
+ * 合并两个有序链表:将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ **/
+public class pro21 {
+    //类似归并排序中的合并过程
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //虚拟头节点
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        // 任一为空，直接连接另一条链表
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
+    }
+
+    //递归
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next=mergeTwoLists2(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
