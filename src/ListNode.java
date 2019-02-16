10eb5798786d286cb873799d851a1350dc66c9ed
JDawnF <335825732@qq.com>
2019-02-16 22:38:45 +0800

    新增打印链表方法

--- src/ListNode.java
+++ src/ListNode.java
@@ -10,4 +10,12 @@ public class ListNode {
         val = x;
         next = null;
     }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
 }
