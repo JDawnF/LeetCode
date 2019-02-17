67dfd61898b88a0f0bf6d79c11fd2d2e84e5d362
JDawnF <335825732@qq.com>
2019-02-17 21:16:23 +0800

    给定一个链表，判断链表中是否有环。

---
+++ src/linkedlist/pro141.java
@@ -0,0 +1,38 @@
+package linkedlist;
+
+
+/**
+ * @program: leetcode
+ * @author: baichen
+ * 给定一个链表，判断链表中是否有环。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。
+ * <p>
+ * 思路：
+ * 使用快慢指针，快指针每次走两步，慢指针每次走一步，两者相遇则有环链表
+ **/
+public class pro141 {
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(3);
+        root.next.next.next = new ListNode(4);
+        root.next.next.next.next = root.next.next;
+        hasCycle(root);
+    }
+}
