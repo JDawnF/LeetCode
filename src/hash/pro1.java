c96b182e8a66345217e1d6039123fd75f64db21d
JDawnF <335825732@qq.com>
2019-02-26 14:15:17 +0800

    两数相加

--- src/hash/pro1.java
+++ src/hash/pro1.java
@@ -1,3 +1,5 @@
+package hash;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,6 +8,14 @@ import java.util.Map;
  * @author: baichen
  * 给定一个整数数组 nums 和一个目标值 target，
  * 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 解题思路：
+ * 可以先对数组进行排序，然后使用双指针方法或者二分查找方法。
+ * 这样做的时间复杂度为 O(NlogN)，空间复杂度为 O(1)。
+ *
+ * 用 HashMap 存储数组元素和索引的映射，在访问到 nums[i] 时，
+ * 判断 HashMap 中是否存在 target - nums[i]，
+ * 如果存在说明 target - nums[i] 所在的索引和 i 就是要找的两个数。
+ * 该方法的时间复杂度为 O(N)，空间复杂度为 O(N)，使用空间来换取时间。
  **/
 public class pro1 {
     public int[] twoSum(int[] nums, int target) {
