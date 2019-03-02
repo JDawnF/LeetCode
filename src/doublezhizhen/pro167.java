1d78671ca9b22ea7a82a5b9af00a9ea38a7790cf
JDawnF <335825732@qq.com>
2019-03-02 23:23:29 +0800

    两数之和 II - 输入有序数组

---
+++ src/doublezhizhen/pro167.java
@@ -0,0 +1,34 @@
+package doublezhizhen;
+
+/**
+ * @program: leetcode
+ * @author: baichen
+ * 两数之和 II - 输入有序数组
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * 说明:
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 解题思路：
+ * 使用双指针，一个指针指向值较小的元素，一个指针指向值较大的元素。指向较小元素的指针从头向尾遍历，指向较大元素的指针从尾向头遍历。
+ * <p>
+ * - 如果两个指针指向元素的和 sum == target，那么得到要求的结果；
+ * - 如果 sum > target，移动较大的元素，使 sum 变小一些；
+ * - 如果 sum < target，移动较小的元素，使 sum 变大一些。
+ **/
+public class pro167 {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return null;
+    }
+}
