149045844566319e1562ea55074fc97d34b85164
JDawnF <335825732@qq.com>
2019-03-14 21:33:08 +0800

    计算给定二叉树的所有左叶子之和。

---
+++ src/tree/pro404.java
@@ -0,0 +1,32 @@
+package tree;
+
+/**
+ * @program: leetcode
+ * @author: baichen
+ * 计算左叶子之和
+ * 计算给定二叉树的所有左叶子之和。
+ * <p>
+ * 示例：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ * 解题思路：
+ * 创建一个判断一个节点是否为叶子结点的方法
+ **/
+public class pro404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (isLeaf(root.left)) return root.left.val + sumOfLeftLeaves(root.right);
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+    private boolean isLeaf(TreeNode node) {
+        if (node == null) return false;
+        return node.left == null && node.right == null;
+    }
+}
