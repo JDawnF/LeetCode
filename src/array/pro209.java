0c2dde57d6f2e0d38797b11157d7371af2547a7c
JDawnF <335825732@qq.com>
2019-06-13 23:58:09 +0800

    长度最小的子数组

--- src/array/pro209.java
+++ src/array/pro209.java
@@ -21,21 +21,6 @@ package array;
  */
 public class pro209 {
     public int minSubArrayLen(int s, int[] nums) {
-        int sum = 0, l = 0, r = -1, res = nums.length - 1;
-        while (l < nums.length) {
-            if (r + 1 < nums.length && sum < s)
-                sum += nums[++r];
-            else
-                sum -= nums[l++];
-            if (sum >= s)
-                res = Math.min(res, r - l + 1);
-        }
-        if (res == nums.length + 1)
-            return 0;
-        return res;
-    }
-    // 滑动窗口
-    public int minSubArrayLen2(int s, int[] nums) {
         int i = 0;
         int sum = 0;
         int len = 0;
